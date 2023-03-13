f9b660f652e8cfb08a5027e626e9a09be99194d9
Mateusz Galaś <119001839+MateuszGalas@users.noreply.github.com>
2023-03-13 22:33:00 +0100

    Add files via upload

---
+++ Format a table/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a = readln().toDouble()
+    val b = readln().toDouble()
+    val c = readln().toDouble()
+    val d = readln().toDouble()
+
+    print(String.format("%,12.2f", a))
+    print(String.format("%,12.2f\n", b))
+    print(String.format("%,12.2f", c))
+    print(String.format("%,12.2f", d))
+}
