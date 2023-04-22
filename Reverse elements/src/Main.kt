0266e2bf3f1335b134d93822276c74fc8245c18d
Mateusz Galaś <119001839+MateuszGalas@users.noreply.github.com>
2023-04-22 19:25:29 +0200

    Add files via upload

---
+++ Reverse elements/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val originalList = readln().split(" ").map { it }.toMutableList()
+    val originalSet = originalList.toMutableSet()
+
+    println(addElements(originalList))
+    println(addElements(originalSet))
+}
+
+fun addElements(list: Collection<String>): Collection<String> {
+    return list.reversed()
+}
