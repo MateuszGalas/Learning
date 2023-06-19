6a2bbe6896c4623895b82dda26fe08823b79632b
Mateusz Galaś <119001839+MateuszGalas@users.noreply.github.com>
2023-06-19 16:58:26 +0200

    Add files via upload

--- Hacking the Matrix/src/Main.kt
+++ Hacking the Matrix/src/Main.kt
@@ -1,16 +1,11 @@
 fun main() {
-    var list = readln().split(" ").map { it.toInt() }
-    val list2 = mutableListOf<Int>()
-
-    list.forEachIndexed { index, _ ->
-        list = if (index % 2 == 0) {
-            list2.add(list[0])
-            list.drop(1)
-        } else {
-            list2.add(list[list.lastIndex])
-            list.dropLast(1)
-        }
-    }
-
-    println(list2.joinToString(" "))
+    readln()
+        .split(" ")
+        .map { it.toInt() }
+        .asSequence()
+        .filter { it % 2 == 1 }
+        .map { it * 2 }
+        .chunked(2) { it.sum() }
+        .toList()
+        .let(::println)
 }
